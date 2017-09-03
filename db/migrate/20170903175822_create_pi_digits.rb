386296ae82722da4188ccb42433001d911a463bd
JesseChamberlain <chamberlain.jesse@gmail.com>
2017-09-03 16:31:54 -0400

    Migrations and models created, Seeding successful

--- db/migrate/20170903175822_create_pi_digits.rb
+++ db/migrate/20170903175822_create_pi_digits.rb
@@ -1,18 +1,18 @@
 class CreatePiDigits < ActiveRecord::Migration[5.1]
   def change
     create_table :pi_digits do |t|
-      t.integer :digit, null: false
-      t.integer :two_digits, null: false
-      t.integer :three_digits, null: false
-      t.integer :four_digits, null: false
-      t.integer :five_digits, null: false
-      t.integer :six_digits, null: false
-      t.integer :seven_digits, null: false
-      t.integer :eight_digits, null: false
-      t.integer :nine_digits, null: false
-      t.integer :ten_digits, null: false
+      t.integer :digit, limit: 8, null: false
+      t.integer :two_digits, limit: 8, null: true
+      t.integer :three_digits, limit: 8, null: true
+      t.integer :four_digits, limit: 8, null: true
+      t.integer :five_digits, limit: 8, null: true
+      t.integer :six_digits, limit: 8, null: true
+      t.integer :seven_digits, limit: 8, null: true
+      t.integer :eight_digits, limit: 8, null: true
+      t.integer :nine_digits, limit: 8, null: true
+      t.integer :ten_digits, limit: 8, null: true
     end
-    
+
     add_index :pi_digits, :digit
   end
 end
