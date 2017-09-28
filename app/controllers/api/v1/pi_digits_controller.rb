fced31a0389fa643dfb8c35564bfb40e8a498cc5
JesseChamberlain <chamberlain.jesse@gmail.com>
2017-09-28 10:35:24 -0400

    Madness

--- app/controllers/api/v1/pi_digits_controller.rb
+++ app/controllers/api/v1/pi_digits_controller.rb
@@ -10,13 +10,14 @@ class Api::V1::PiDigitsController < ApplicationController
     pi_length = 99980
     initial_index = 2
     # known loops at index=19 & index=40
-    initial_int = Pi_digit.find(40)
+    initial_int = Pi_digit.find(2)
     digit_at_id_index = 0
     zero_found = 0
     def search_single_loop(i, digit_i, z, int, len, all, single)
       break_out = false
       while i < len && break_out == false
         digits_at_id = [
+          int["digit"],
           int["two_digits"],
           int["three_digits"],
           int["four_digits"],
@@ -26,8 +27,16 @@ class Api::V1::PiDigitsController < ApplicationController
           int["eight_digits"],
           int["nine_digits"]
         ]
-        while digit_i < 8
-          binding.pry
+        while digit_i < 9
+          # Chances are this will need to incorporate a complex digit counter for
+          # the digit count in each single index.
+          if single.length == 22
+            if single[0]["start"] > 40
+              binding.pry
+            end
+            int = Pi_digit.find(single[0]["start"]+1)
+            single = []
+          end
           if 0 < z
             digit = digits_at_id[z]
           else
@@ -35,7 +44,7 @@ class Api::V1::PiDigitsController < ApplicationController
           end
           id = int.id
           id_hash = {}
-          unless digit == 0
+          if digit != 0
             z = 0
             if single[0] == nil
               single.push({"start" => id})
@@ -76,11 +85,12 @@ class Api::V1::PiDigitsController < ApplicationController
               end
             end
             search_single_loop(i, digit_i, z, int, len, all, single)
-          end
-          single.last.each do | key, value |
-            single.last[key] = nil
-            int = Pi_digit.find(key)
-            z += 1
+          else
+            single.last.each do | key, value |
+              z = (value.digits.count)
+              single.last[key] = nil
+              int = Pi_digit.find(key)
+            end
           end
           search_single_loop(i, digit_i, z, int, len, all, single)
         end
